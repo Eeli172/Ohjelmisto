36aa320a25233f44eff9e3db0a6dc4f405d588fd
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-01-16 12:21:07 +0200

    tehtävä

---
+++ mod-02/02_t4.py
@@ -0,0 +1,10 @@
+luku1_int = int(input("Anna luku: "))
+luku2_int = int(input("Anna toinen luku: "))
+luku3_int = int(input("Anna kolmas luku: "))
+
+summa_int = int(luku1_int + luku2_int + luku3_int)
+tulo_int  = int(luku1_int * luku2_int * luku3_int)
+keskiarvo_float = float((luku1_int + luku2_int + luku3_int) / 3;)
+
+print(f"Antamiesi lukujen summa on {summa_int}, tulo on {tulo_int} ja keskiarvo on {keskiarvo_float}")
+
