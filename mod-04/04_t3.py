0d36ada8adcd60b96b5405965f78d1ecba66522f
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-01-21 22:01:07 +0200

    tehtävät

--- mod-04/04_t3.py
+++ mod-04/04_t3.py
@@ -1,29 +1,44 @@
 list_of_numbers = []
-def to_integer(s): 
+
+# tarkistaa, onko kyseessä integer, palauttaa boolean -arvon
+def is_integer(variable): 
     try: 
-        int(s) 
+        int(variable) 
         return True 
     except ValueError: 
         return False 
-def to_float(s): 
+    
+# tarkistaa, onko kyseessä float, palauttaa boolean -arvon
+def is_float(variable): 
     try: 
-        float(s) 
+        float(variable) 
         return True 
     except ValueError: 
         return False 
 
 while True:
     inp = input("Anna luku: ")
+
+# käyttäjän syöttämä tyhjä merkkijono katkaisee loopin
     if inp == "":
         print("Syötit tyhjän merkkijonon, ohjelma sulkeutuu.")
         break
-    elif to_integer(inp):
+
+# integer sekä float -arvoiksi hyväksytyt syötteet lisätään listalle
+    elif is_integer(inp):
         inp = int(inp)
         list_of_numbers.append(inp)
-    elif to_float(inp):
+    elif is_float(inp):
         inp = float(inp)
         list_of_numbers.append(inp)
 
-print(list_of_numbers)
-print(f"Pienin antamasi numero: {min(list_of_numbers)}")
-print(f"Suurin antamasi numero: {max(list_of_numbers)}")
+# muun muotoiset syötteet palauttavat käyttäjälle virheilmoituksen
+    else:
+        print("Syötit virheellisen arvon, yritä uudelleen. Tyhjä merkkijono päättää ohjelman. ")
+
+
+try: 
+    print(f"Pienin antamasi numero: {min(list_of_numbers)}")
+    print(f"Suurin antamasi numero: {max(list_of_numbers)}")
+except ValueError:
+    print("Et syöttänyt yhtään lukua.")
