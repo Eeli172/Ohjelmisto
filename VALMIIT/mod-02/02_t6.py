da078140aeb90e84fd531eb77d01a9720c91f233
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-01-21 14:14:14 +0200

    tehtävä

--- VALMIIT/mod-02/02_t6.py
+++ VALMIIT/mod-02/02_t6.py
@@ -10,5 +10,11 @@ while loop < 3:
     neljä += str(random.randint(1,6))
     loop += 1
 
+
+# fiksumpi tapa: 
+# kolme = "".join(str(random.randint(0,9)) for i in range(3))
+# neljä = "".join(str(random.randint(1,6)) for i in range(4))
+
+
 print(f"kolmenumeroinen koodi: {kolme}")
 print(f"nelinumeroinen koodi: {neljä}")
