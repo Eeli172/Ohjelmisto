a66ac5016e70b5a2eec5a24aba0e1ab5db1e00c0
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-01-23 12:12:50 +0200

    tehtävät

--- mod-04/04_t4.py
+++ mod-04/04_t4.py
@@ -1,10 +1,11 @@
 import random
 
 randomi = random.randint(1,10)
+arvaus = 0
 
-while 1:
+while randomi != arvaus:
     arvaus = int(input("\nArvaa tietokoneen arpoma kokonaisluku väliltä 1 ja 10: "))
     if randomi == arvaus:
         print(f"\n\nArvasit oikein, tietokoneen arpoma luku oli {randomi}\n\n")
-        break
-    print("\nVäärä arvaus, yritä uudelleen. ")
+    else:
+        print("\nVäärä arvaus, yritä uudelleen. ")
