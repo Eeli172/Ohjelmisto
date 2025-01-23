6789e88af957e924df49fff90aeeb43105686e92
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-01-23 11:32:11 +0200

    tehtävät

--- mod-04/04_t3_tapa1.py
+++ mod-04/04_t3_tapa1.py
@@ -1,21 +1,23 @@
-pienin = ""
-suurin = ""
-print("\nSyötä lukuja, ohjelma kerää näistä suurimman ja pienimmän. \nPäätä ohjelma syöttämällä tyhjä merkkijono. \nLopussa ohjelma tulostaa syöttämistäsi luvuista pienimmän ja suurimman.\n")
+import math
 
-while 1:
-    x = str(input("Syötä luku: "))
-    if x == "":
-        print("\nSyötit tyhjän merkkijonon, ohjelma päättyy.")
-        break
-    elif pienin == "":
-        pienin = float(x)
-        suurin = float(x)
-    elif float(pienin) > float(x):
-        pienin = float(x)
-    elif float(suurin) < float(x):
-        suurin = float(x)
+pienin = math.inf
+suurin = -math.inf
 
-if type(pienin) == float:
+syöte_str = str(input("\nAnna ensimmäinen luku: "))
+
+while syöte_str != "":
+
+    syöte = float(syöte_str)
+
+    if pienin > syöte:
+        pienin = syöte
+
+    if suurin < syöte:
+        suurin = syöte
+
+    syöte_str = str(input("\nAnna uusi luku"))
+
+if pienin != math.inf:
     print(f"\nPienin syöttämäsi luku on {pienin} ja suurin syöttämäsi luku on {suurin}\n")
 else: 
-    print("Et syöttänyt yhtään lukua.")
+    print("\nEt syöttänyt yhtään lukua.\n")
