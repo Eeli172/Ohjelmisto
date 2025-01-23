10f5969a7cbcb2d405ec9cfd8b5c9d55e5e84f92
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-01-23 14:15:42 +0200

    tehtävät

---
+++ mod-08/08_t1.py
@@ -0,0 +1,5 @@
+# Huom! Joillain käyttäjillä on ilmennyt yllättäviä haasteita uusimman MySQL-ajuriversion 8.0.30 kanssa. Jos törmäät virheilmoitukseen mysql.connector.errors.ProgrammingError: Character set 'utf8' unsupported, vaihda toiseksi uusimpaan ajuriversioon 8.0.29: Valitse PyCharmissa View/Tool Windows/Python Packages. Etsi hakutoiminnolla pakkaus mysql-connector-python. Poista version 8.0.30 asennus painamalla oikeassa laidassa olevaa kolmea pistettä ja valitsemalla Delete. Vaihda ajuriversioksi 8.0.29 Latest-valinnan tilalle ja asenna napsauttamalla Install.
+
+# Kirjoita ohjelma, joka kysyy käyttäjältä lentoaseman ICAO-koodin. 
+# Ohjelma hakee ja tulostaa koodia vastaavan lentokentän nimen ja sen sijaintikunnan kurssilla käytettävästä lentokenttätietokannasta. 
+# ICAO-koodi on tallennettuna airport-taulun ident-sarakkeeseen.
---
+++ mod-08/08_t2.py
@@ -0,0 +1,5 @@
+# Huom! Joillain käyttäjillä on ilmennyt yllättäviä haasteita uusimman MySQL-ajuriversion 8.0.30 kanssa. Jos törmäät virheilmoitukseen mysql.connector.errors.ProgrammingError: Character set 'utf8' unsupported, vaihda toiseksi uusimpaan ajuriversioon 8.0.29: Valitse PyCharmissa View/Tool Windows/Python Packages. Etsi hakutoiminnolla pakkaus mysql-connector-python. Poista version 8.0.30 asennus painamalla oikeassa laidassa olevaa kolmea pistettä ja valitsemalla Delete. Vaihda ajuriversioksi 8.0.29 Latest-valinnan tilalle ja asenna napsauttamalla Install.
+# 
+# Kirjoita ohjelma, joka kysyy käyttäjältä maakoodin (esimerkiksi FI) ja tulostaa kyseisessä maassa olevien lentokenttien lukumäärät tyypeittäin. 
+# Esimerkiksi Suomen osalta tuloksena on saatava tieto siitä, että pieniä lentokenttiä on 65 kappaletta, 
+# helikopterikenttiä on 15 kappaletta jne.
