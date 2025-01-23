e2b21469bd8963032fef5e902a55af4fb252729c
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-01-23 15:33:19 +0200

    tehtävät

--- mod-05/05_t2.py
+++ mod-05/05_t2.py
@@ -2,3 +2,22 @@
 # Lopuksi ohjelma tulostaa saaduista luvuista viisi suurinta suuruusjärjestyksessä suurimmasta alkaen. 
 #   Vihje: listan alkioiden lajittelujärjestyksen voi kääntää antamalla sort-metodille argumentiksi reverse=True.
 
+lista = []
+syöte_str = str(input("Syötä ensimmäinen luku: "))
+
+while syöte_str != "":
+    syöte_float = float(syöte_str)
+    lista.append(syöte_float)
+    syöte_str = str(input("Syötä uusi luku: "))
+
+lista.sort(reverse=True)
+
+if len(lista) >= 5:
+    print("Viisi suurinta syöttämääsi numeroa järjestettynä suurimmasta pienimpään")
+
+    for i in range(0,5):
+        print(lista[i])
+else:
+    print(f"{len(lista)} suurinta syöttämääsi numeroa järjestettynä suurimmasta pienimpään")
+    for i in range(0,len(lista)):
+        print(lista[i])
