d87bfaf70d1471fbd1cf94a47697ee9973af7b5e
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-01-23 14:26:41 +0200

    tehtävät

--- mod-05/05_t1.py
+++ mod-05/05_t1.py
@@ -1,3 +1,9 @@
-# Kirjoita ohjelma, joka kysyy käyttäjältä arpakuutioiden lukumäärän. 
-# Ohjelma heittää kerran kaikkia arpakuutioita ja tulostaa silmälukujen summan. Käytä for-toistorakennetta.
+import random
 
+arpakuutio_lkm = int(input("Anna arpakuutioiden lukumäärä: "))
+summa = 0
+
+for i in range(arpakuutio_lkm):
+    summa += random.randint(1,6)
+
+print(f"{arpakuutio_lkm} kpl noppia heitetty, saatujen silmälukujen summa: {summa}")
