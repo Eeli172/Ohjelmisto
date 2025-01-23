643f3bef61364f1c680abbff7df0c44f1fa5aa67
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-01-23 12:36:06 +0200

    tehtävät

--- mod-04/04_t5.py
+++ mod-04/04_t5.py
@@ -1,9 +1,18 @@
-"""
-Kirjoita ohjelma, joka kysyy käyttäjältä käyttäjätunnuksen ja salasanan. 
-Jos jompikumpi tai molemmat ovat väärin, tunnus ja salasana kysytään uudelleen. 
-Tätä jatketaan kunnes kirjautumistiedot ovat oikein tai väärät tiedot on syötetty viisi kertaa. 
-Edellisessä tapauksessa tulostetaan Tervetuloa ja jälkimmäisessä Pääsy evätty. 
-(Oikea käyttäjätunnus on python ja salasana rules).
-"""
+kt = "python"
+ss = "rules"
+kt_a = ""
+ss_a = ""
+x = 0
 
+while kt != kt_a and ss != ss_a and x < 5:
+
+    kt_a = input("Syötä käyttäjätunnus: ")
+    ss_a = input("Syötä salasana: ")
+    x += 1
+    if kt == kt_a and ss == ss_a:
+        print("\nTervetuloa!\n")
+    elif x == 5:
+        print("\nOlet syöttänyt virheellisen käyttäjätunnuksen tai salasanan viisi kertaa, yritä myöhemmin uudelleen.\n")
+    else:
+        print(f"\nPääsy evätty, yritä uudelleen. \nTämä oli {x}. yrityksesi. Maksimi yritykset: 5\n")
 
