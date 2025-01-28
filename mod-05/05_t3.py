7a6a366e0f2831bd0253e29645820cb44c6e9e60
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-01-28 14:19:45 +0200

    tehtävät

--- mod-05/05_t3.py
+++ mod-05/05_t3.py
@@ -1,17 +1,14 @@
-# Kirjoita ohjelma, joka kysyy käyttäjältä kokonaisluvun ja ilmoittaa, onko se alkuluku. 
-# Tässä tehtävässä alkulukuja ovat luvut, jotka ovat jaollisia vain ykkösellä ja itsellään.
-#   Esimerkiksi luku 13 on alkuluku, koska se voidaan jakaa vain luvuilla 1 ja 13 siten, että jako menee tasan.
-#   Toisaalta esimerkiksi luku 21 ei ole alkuluku, koska se voidaan jakaa tasan myös luvulla 3 tai luvulla 7.
-
 kokonaisluku = int(input("Syötä kokonaisluku: "))
-helper = 2
 alkuluku = True
 
-while alkuluku and helper < kokonaisluku-1:
-    if kokonaisluku % helper == 0:
-        alkuluku = False
-
-    helper += 1
+if kokonaisluku > 0:
+    for i in range(2, kokonaisluku):
+        if kokonaisluku % i == 0:
+            alkuluku = False
+else:
+    for i in range(2, -kokonaisluku):
+        if kokonaisluku % i == 0:
+            alkuluku = False
 
 if alkuluku:
     print("Syöttämäsi luku on alkuluku. ")
