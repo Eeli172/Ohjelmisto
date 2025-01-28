6b3e3687a9128b79eee555198f1e6a5724e5626d
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-01-28 14:23:02 +0200

    tehtävät

--- mod-05/05_t3.py
+++ mod-05/05_t3.py
@@ -1,14 +1,9 @@
 kokonaisluku = int(input("Syötä kokonaisluku: "))
 alkuluku = True
 
-if kokonaisluku > 0:
-    for i in range(2, kokonaisluku):
-        if kokonaisluku % i == 0:
-            alkuluku = False
-else:
-    for i in range(2, -kokonaisluku):
-        if kokonaisluku % i == 0:
-            alkuluku = False
+for i in range(2, kokonaisluku):
+    if kokonaisluku % i == 0:
+        alkuluku = False
 
 if alkuluku:
     print("Syöttämäsi luku on alkuluku. ")
