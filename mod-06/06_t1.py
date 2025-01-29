71c1135b4c7ac8d24607fdc9ac5396ae9ce4c5aa
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-01-29 14:26:57 +0200

    tehtävät

--- mod-06/06_t1.py
+++ mod-06/06_t1.py
@@ -1,3 +1,15 @@
 # Kirjoita parametriton funktio, joka palauttaa paluuarvonaan satunnaisen nopan silmäluvun väliltä 1..6. 
 # Kirjoita pääohjelma, joka heittää noppaa niin kauan kunnes tulee kuutonen. 
 # Pääohjelma tulostaa kunkin heiton jälkeen saadun silmäluvun.
+
+import random
+
+def noppa():
+    x = int(random.randint(1,6))
+    return x
+
+tulos = 0
+
+while tulos != 6:
+    tulos = noppa()
+    print(f"Nopasta saatu silmäluku: {tulos}")
