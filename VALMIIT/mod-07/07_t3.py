96f53bb6167f58204cb1ae97886d6498a9dab965
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-02-04 21:11:09 +0200

    tehtävät

--- VALMIIT/mod-07/07_t3.py
+++ VALMIIT/mod-07/07_t3.py
@@ -1,15 +1,13 @@
-db = set()
+db = {}
 ip = int(input("Haluatko: syöttää uuden lentoaseman [1], hakea jo syötetyn lentoaseman tiedot [2], vai lopettaa [3]: "))
 while ip != 3:
     if ip == 1:
         icao = str(input("Syötä lentoaseman ICAO-koodi: "))
         nimi = str(input("Syötä lentoaseman nimi: "))
-        db.add((icao, nimi))
+        db[icao] = nimi
     elif ip == 2:
         haku = str(input("Syötä haettavan lentoaseman ICAO-koodi: "))
-        for i in db:
-            if i[0] == haku:
-                print(i[1])
+        print(db[haku])
     else:
         print("Annoit virheellisen syötteen, anna jokin seuraavista numeroista [1, 2, 3]")
     ip = int(input("Haluatko: syöttää uuden lentoaseman [1], hakea jo syötetyn lentoaseman tiedot [2], vai lopettaa [3]: "))
