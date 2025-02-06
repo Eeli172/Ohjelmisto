a54b1bf0cc30474ead83f55e4e1c53699971d050
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-02-06 09:31:44 +0200

    tehtävät

--- mod-08/08-t1.py
+++ mod-08/08-t1.py
@@ -3,3 +3,33 @@
 # Kirjoita ohjelma, joka kysyy käyttäjältä lentoaseman ICAO-koodin. 
 # Ohjelma hakee ja tulostaa koodia vastaavan lentokentän nimen ja sen sijaintikunnan kurssilla käytettävästä lentokenttätietokannasta. 
 # ICAO-koodi on tallennettuna airport-taulun ident-sarakkeeseen.
+import mysql.connector
+
+
+def hae_kentan_tiedot(icao):
+    sql = f"SELECT name FROM airport where ident='{icao}'"
+    print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount > 0:
+        for rivi in tulos:
+            print(f"Lentoaseman nimi on: {rivi[0]}")
+    else:
+        print("ICAO-koodillasi ei löytynyt lentoasemaa.")
+    return
+
+
+# pääohjelma
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port= 3306,
+    database='flight_game',
+    # Huom: käyttäjän root salasana EI saa olla root!!!
+    user='root',
+    password='root',
+    autocommit=True
+    )
+
+icao_koodi = input("Anna lentoaseman ICAO-koodi: ")
+hae_kentan_tiedot(icao_koodi)
