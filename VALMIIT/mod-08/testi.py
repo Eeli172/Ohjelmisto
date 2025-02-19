15f448052fdf9dd693a5fa73495293d980d7086a
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-02-19 15:30:53 +0200

    alustavaa pohdintaa ja selvitystyötä

--- VALMIIT/mod-08/testi.py
+++ VALMIIT/mod-08/testi.py
@@ -1,6 +1,13 @@
-koko = (int(input("Anna luku: ")) // 2)
-x = int(1)
-st = "*"
-for z in range(koko):
-    print(f"{st*x:>(koko)s}")
-    x += 2
+from sympy import *
+
+a = symbols("a")
+b = symbols("b")
+c = symbols("c")
+x = symbols("x")
+y = symbols("y")
+nimittäjä = input("Syötä nimittäjän arvo: ")
+nimittäjä = sympify(nimittäjä)
+osoittaja = input("Syötä osoittajan arvo: ")
+osoittaja = sympify(osoittaja)
+
+print(f"Tulos: {factor((nimittäjä)/(osoittaja))}")
