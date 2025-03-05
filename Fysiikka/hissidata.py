7e274926f39cf8b18e565ce9b9d60a030024a15e
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-03-05 11:39:04 +0200

    Harjoiitustehtävä

--- Fysiikka/hissidata.py
+++ Fysiikka/hissidata.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 
 with open('hissidata.csv', mode = 'r') as hissidata:
     data = csv.reader(hissidata)
+
+    # Kiihtyvyyden ja ajan erottelu omiin listoihin
     time = [0]
     acc = [0]
     for row in data:
@@ -12,8 +14,41 @@ with open('hissidata.csv', mode = 'r') as hissidata:
             acc.append(float(row[1]))
         except ValueError:
             pass
+    
+    # Kiihtyvyyden määrittely
+    vel = [0]
+    for i in range(len(acc)-1):
+        ak = (acc[i]+acc[i+1])/2 # keskimääräinen kiihtyvyys
+        dt = time[i+1]-time[i] # aikavälin pituus
+        vel.append(vel[-1] + ak*dt)
 
-    plt.xlabel('t (s)')
-    plt.ylabel('a (m/s$^{2}$)')
-    plt.plot(time,acc)
-    plt.show()
+    # Siirtymän määrittely
+    x = [0]
+    for i in range(len(vel)-1):
+        vk = (vel[i]+vel[i+1])/2 # keskimääräinen nopeus
+        dt = time[i+1]-time[i]
+        x.append(x[-1] + vk*dt)
+
+    inp = int(input("    1 - Kiihtyvyys-graafi\n    2 - Nopeus-graafi\n    3 - Siirtymä-graafi\n    0 - Sulje ohjelma\n\nValitse edellisistä: "))
+    while inp != 0:
+        if inp == 1: # Kiihtyvyys-graafi
+            plt.xlabel('t (s)')
+            plt.ylabel('a (m/s$^{2}$)')
+            plt.plot(time,acc)
+            plt.show()
+    
+        elif inp == 2: # Nopeus-graafi
+            plt.xlabel('t (s)')
+            plt.ylabel('v (m/s)')
+            plt.plot(time,vel)
+            plt.show()
+        
+        elif inp == 3: # Siirtymä-graafi 
+            plt.xlabel('t (s)')
+            plt.ylabel('x (m)')
+            plt.plot(time,x)
+            plt.show()
+
+        else:
+            print("Syötit määrittämättömän arvon, yritä uudelleen")
+        inp = int(input("    1 - Kiihtyvyys-graafi\n    2 - Nopeus-graafi\n    3 - Siirtymä-graafi\n    0 - Sulje ohjelma\n\nValitse edellisistä: "))
