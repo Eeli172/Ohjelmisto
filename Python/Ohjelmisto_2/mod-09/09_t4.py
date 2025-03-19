7c372e8f2bda091675a826c39d7ab82913c00566
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-03-19 17:37:58 +0200

    tilastot

--- Python/Ohjelmisto_2/mod-09/09_t4.py
+++ Python/Ohjelmisto_2/mod-09/09_t4.py
@@ -1,11 +1,5 @@
-# Nyt ohjelmoidaan autokilpailu. Uuden auton kuljettu matka alustetaan automaattisesti nollaksi. 
-# Tee pääohjelman alussa lista, joka koostuu kymmenestä toistorakenteella luodusta auto-oliosta. 
-# Jokaisen auton huippunopeus arvotaan 100 km/h ja 200 km/h väliltä. Rekisteritunnus luodaan seuraavasti "ABC-1", "ABC-2" jne. 
-# Sitten kilpailu alkaa. Kilpailun aikana tehdään tunnin välein seuraavat toimenpiteet:
-#       - Jokaisen auton nopeutta muutetaan siten, että nopeuden muutos arvotaan väliltä -10 ja +15 km/h väliltä. 
-#         Tämä tehdään kutsumalla kiihdytä-metodia.
-#       - Kaikkia autoja käsketään liikkumaan yhden tunnin ajan. Tämä tehdään kutsumalla kulje-metodia.
-# 
+
+
 # Kilpailu jatkuu, kunnes jokin autoista on edennyt vähintään 10000 kilometriä. 
 # Lopuksi tulostetaan kunkin auton kaikki ominaisuudet selkeäksi taulukoksi muotoiltuna.
 import random
@@ -46,5 +40,9 @@ def pääohjelma():
                 print(f'{auto.rekisteritunnus} voitti kilpailun!')
                 kilpailu_käynnissä = False
 
+    # tilastojen tulostus
+    print("\nrek.nro.    huippunopeus    kuljettu matka\n")
+    for auto in auto_lista:
+        print(auto.rekisteritunnus, "     ", auto.huippunopeus, "           ", auto.kuljettu_matka)
 
 pääohjelma()
