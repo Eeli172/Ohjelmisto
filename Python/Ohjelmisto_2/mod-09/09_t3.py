fb5a64c236a33a7ca439a5fb962795bdc34ab110
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-03-19 15:09:40 +0200

    tehtävä 3 tehty

--- Python/Ohjelmisto_2/mod-09/09_t3.py
+++ Python/Ohjelmisto_2/mod-09/09_t3.py
@@ -3,3 +3,39 @@
 # Esimerkki: auto-olion tämänhetkinen kuljettu matka on 2000 km. Nopeus on 60 km/h. 
 # Metodikutsu auto.kulje(1.5) kasvattaa kuljetun matkan lukemaan 2090 km.
 
+class Auto:
+    def __init__(self, rekisteritunnus, huippunopeus, nykyinen_nopeus=0, kuljettu_matka=0):
+        Auto.rekisteritunnus = rekisteritunnus
+        Auto.huippunopeus = huippunopeus
+        Auto.nykyinen_nopeus = nykyinen_nopeus
+        Auto.kuljettu_matka = kuljettu_matka
+
+    def kiihdytä(self, nopeuden_muutos):
+        if self.nykyinen_nopeus + nopeuden_muutos < 0:# jos nopeus menisi alle 0, muutetaan nyk.nopeus -> 0
+            self.nykyinen_nopeus = 0 
+        elif self.nykyinen_nopeus + nopeuden_muutos > self.huippunopeus:# jos nopeus menisi yli huippunopeuden, muutetaan nyk.nopeus -> huippunopeus
+            self.nykyinen_nopeus = self.huippunopeus
+        else:# muuten lisätään nopeuden muutos nykyiseen nopeuteen
+            self.nykyinen_nopeus += nopeuden_muutos
+
+    def kulje(self, tunnit):
+        self.kuljettu_matka = self.nykyinen_nopeus * tunnit
+
+
+def pääohjelma():
+    auto_lista = []
+    auto_lista.append(Auto("ABC-123", 142))
+
+    auto_lista[0].kiihdytä(60)
+    print(f'Auton nopeus: {auto_lista[0].nykyinen_nopeus} km/h')
+
+    print(f'Kuljettu matka alussa: {auto_lista[0].kuljettu_matka} km')
+
+    print(f'Autolla matkustetaan: 1,5 h')
+    auto_lista[0].kulje(1.5)
+
+    print(f'Kuljettu matka lopussa: {auto_lista[0].kuljettu_matka} km')
+
+
+
+pääohjelma()
