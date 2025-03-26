ebb539cdcd4b6b81758ef20002f44e70c48d83cf
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-03-26 17:38:00 +0200

    done

--- Python/Ohjelmisto_2/mod-12/12_t1.py
+++ Python/Ohjelmisto_2/mod-12/12_t1.py
@@ -1,2 +1,13 @@
-# Kirjoita ohjelma, joka hakee ja tulostaa satunnaisen Chuck Norris -vitsin käyttäjälle. 
-# Käytä seuravalla sivulla esiteltävää rajapintaa: https://api.chucknorris.io/. Käyttäjälle on näytettävä pelkkä vitsin teksti.
+import requests
+
+
+pyyntö = "https://api.chucknorris.io/jokes/random"
+
+try: 
+    vastaus = requests.get(pyyntö)
+    if vastaus.status_code==200:
+        json_vastaus = vastaus.json()
+        print(json_vastaus["value"])
+
+except requests.exceptions.RequestException as e:
+    print("Hakua ei voitu suorittaa.")
--- Python/Ohjelmisto_2/mod-12/12_t2.py
+++ Python/Ohjelmisto_2/mod-12/12_t2.py
@@ -2,3 +2,41 @@
 # Kirjoita ohjelma, joka kysyy käyttäjältä paikkakunnan nimen ja tulostaa sitä vastaavan säätilan tekstin sekä lämpötilan Celsius-asteina. 
 # Perehdy rajapinnan dokumentaatioon riittävästi. Palveluun rekisteröityminen on tarpeen, jotta saat rajapintapyynnöissä tarvittavan API-avaimen (API key). 
 # Selvitä myös, miten saat Kelvin-asteet muunnettua Celsius-asteiksi.
+
+import json
+from os import name
+import requests
+
+api_key = "55c711f6d57383bc1d3e3c07b019180d"
+
+
+location = input("Sijainti: ")
+request = f"http://api.openweathermap.org/geo/1.0/direct?q={location}&limit=1&appid={api_key}"
+try: 
+    answer = requests.get(request)
+    if answer.status_code == 200:
+        answer_json = answer.json()[0]
+        print(f'\nHaetaan säätiedot seuraavaan sijaintiin: {answer_json["name"]}, {answer_json["country"]}\n')
+        lat = answer_json["lat"]
+        lon = answer_json["lon"]
+
+    else: print(answer.status_code)
+
+except requests.exceptions.RequestException as e:
+    print(f"Hakua ei voitu suorittaa. {answer.status_code}")
+
+
+request = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={api_key}"
+try: 
+    answer = requests.get(request)
+    if answer.status_code == 200:
+        answer_json = answer.json()
+        weather = f'Sää:          {answer_json["weather"][0]["main"]}\n'
+        weather += f'Kuvaus:       {answer_json["weather"][0]["description"]}\n'
+        weather += f'Lämpötila:    {answer_json["main"]["temp"]} °C\n'
+        weather += f'Tuntuu kuin:  {answer_json["main"]["feels_like"]} °C'
+        print(weather)
+    else: print(answer.status_code)
+
+except requests.exceptions.RequestException as e:
+    print(f"Hakua ei voitu suorittaa. {answer.status_code}")
