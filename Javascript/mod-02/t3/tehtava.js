45585aa5a318b931f021a0e4395e926d5027daff
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-04-03 11:39:50 +0300

    up

--- Javascript/mod-02/t3/tehtava.js
+++ Javascript/mod-02/t3/tehtava.js
@@ -1,1 +1,4 @@
-// 
+/*
+Write a program that asks for the names of six dogs. The program prints dog names to unordered list <ul> in reverse alphabetical order. (2p)
+
+*/ 
--- Javascript/mod-04/t4/tehtava.js
+++ Javascript/mod-04/t4/tehtava.js
@@ -1,1 +1,5 @@
-// 
+/*
+Develop the app even further. Optional chaining is not the best way to handle missing image. Use ternary operator or if/else to add a default image if TV-show is missing image property. (2p)
+    Use https://via.placeholder.com/210x295?text=Not%20Found as the default image.
+
+*/ 
