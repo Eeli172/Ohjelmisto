9e034fdaf625c708e440680760b9a9ccbcf15b5e
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-04-09 13:50:47 +0300

    up

--- Python/Ohjelmisto_2/mod-13/13_t1.py
+++ Python/Ohjelmisto_2/mod-13/13_t1.py
@@ -1,5 +1,15 @@
-# Toteuta Flask-taustapalvelu, joka ilmoittaa, onko parametrina saatu luku alkuluku vai ei. 
-# Hyödynnä toteutuksessa aiempaa tehtävää, jossa alkuluvun testaus tehtiin. 
-# Esimerkiksi lukua 31 vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/alkuluku/31. 
-# Vastauksen on oltava muodossa: {"Number":31, "isPrime":true}.
+from flask import Flask
+from sympy import isprime
 
+app = Flask(__name__)
+@app.route('/alkuluku/<numero>')
+def alkuluku(numero):
+    numero = int(numero)
+    luku = {
+        "Number": numero,
+        "isPrime": isprime(numero)
+    }
+    return str(luku)
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
